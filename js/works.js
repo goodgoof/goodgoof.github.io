9f3dc972c362d1a21d957d2e0137681bd525a52b
goodgoof <anjalidsharma8@gmail.com>
2019-08-06 12:13:09 -0400

    removed calculator for now
    Changed title description

--- js/works.js
+++ js/works.js
@@ -1,30 +1,30 @@
 var works=[
+// 	{
+// 		title:"First Project-HTML, CSS",
+// 		pic:"img/images_for_web/calculator.png",
+// 		url:"js/calculator.html"
+// 	},
 	{
-		title:"First Project-HTML, CSS",
-		pic:"img/images_for_web/calculator.png",
-		url:"js/calculator.html"
-	},
-	{
-		title:"Second Project-HTML, CSS, Flexbox",
+		title:"HTML, CSS, Flexbox",
 		pic:"img/images_for_web/teaCozy.png",
 		url:"http://trusting-minsky-5f5158.bitballoon.com/"
 	},
 
 	
 	{
-		title:"Third Project- Ruby on Rails",
+		title:"Ruby on Rails",
 		pic:"img/images_for_web/hello-bikes.png",
 		url:"projects/hello-bikes.html"
 	},
 	
 	{
-		title:"Fourth Project-HTML, CSS, Javascript, Bootstrap",
+		title:"HTML, CSS, Javascript, Bootstrap",
 		pic:"img/images_for_web/facebook.png",
 		url:"projects/facebook.html"
 	}, 
 
 	{
-		title:"Fifth Project-HTML, CSS, Flexbox,Responsive",
+		title:"HTML, CSS, Flexbox,Responsive",
 		pic:"img/images_for_web/secretagent.png",
 		url:"projects/Secret-agent-supply.html"
 	}
